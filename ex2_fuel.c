ceaa9b967c27ea935ba02a5d9e59dd52cf9852a2
nambatyabushirat-ux <nambatyabushirat@gmail.com>
2026-03-25 07:46:01 +0300

    Update ex2_fuel.c

--- ex2_fuel.c
+++ ex2_fuel.c
@@ -1,11 +1,21 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
-
+/* Nambatya Bushirat*/
+/* 2500801405: U/BIO/01405/PD */
 #include <stdio.h>
+int main() {
+    float distance, fuel, efficiency;
 
-int main(void)
-{
-    /* Write your code here */
+    // input by the user
+    printf("Enter distance_traveled (in km): ");
+    scanf("%f", &distance);
+
+    printf("Enter fuel_used (in liters): ");
+    scanf("%f", &fuel);
+
+    // Calculate fuel efficiency
+    efficiency = distance / fuel;
+
+    // output
+    printf("Fuel_efficiency: %.2f km/L \n", efficiency);
 
     return 0;
 }
