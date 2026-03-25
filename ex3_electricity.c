4112fb3257d3b4af2d74c3620c03de165954adee
nambatyabushirat-ux <nambatyabushirat@gmail.com>
2026-03-25 07:46:50 +0300

    Update ex3_electricity.c

--- ex3_electricity.c
+++ ex3_electricity.c
@@ -1,11 +1,22 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
-
+/* Nambatya Bushirat*/
+/* 2500801405: U/BIO/01405/PD */
 #include <stdio.h>
 
-int main(void)
-{
-    /* Write your code here */
+int main() {
+    float units, cost_per_unit, total_bill;
+
+    // input by the user 
+    printf("Enter electricity_units_consumed: ");
+    scanf("%f", &units);
+
+    printf("Enter cost_per_unit (in your currency): ");
+    scanf("%f", &cost_per_unit);
+
+    // Calculate total bill
+    total_bill = units * cost_per_unit;
+
+    // output
+    printf("Total_electricity_bill: %.2f \n", total_bill);
 
     return 0;
 }
