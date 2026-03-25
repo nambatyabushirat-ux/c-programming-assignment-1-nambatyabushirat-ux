569a4054a3f349e696f9ce9fd994fb4a85e5057f
nambatyabushirat-ux <nambatyabushirat@gmail.com>
2026-03-25 07:45:07 +0300

    Update ex1_mobile_money.c

--- ex1_mobile_money.c
+++ ex1_mobile_money.c
@@ -1,11 +1,24 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name. : Nambatya Bushirat*/
+/* 2500801405 :25/U/BIO/01405/PD */
 
 #include <stdio.h>
 
-int main(void)
-{
-    /* Write your code here */
+int main() {
+    float amount, fee_percent,fee,total;
+// input by the user
+    printf("Enter amount to send :");
+    scanf("%f",& amount);
+    printf("Enter transaction fee (%):");
+    scanf("%f",&fee_percent);
 
+//calculations
+    fee =(fee_percent/100)*amount;
+    total =amount +fee ;
+
+// outputs
+    printf("transaction_fee : %.2f Ugx \n",fee);
+    printf("total_amount_deducted :%.2f Ugx \n",total);
     return 0;
+
 }
+
